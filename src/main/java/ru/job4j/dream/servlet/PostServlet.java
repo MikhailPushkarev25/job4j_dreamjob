a90408ad588627eee4afc9a977fa286354fabe1a
MikhailPushkarev25 <mixa199525@yandex.ru>
2021-07-27 06:36:29 +0300

    1. HttpSession [#6869]

--- src/main/java/ru/job4j/dream/servlet/PostServlet.java
+++ src/main/java/ru/job4j/dream/servlet/PostServlet.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class PostServlet extends HttpServlet {
 
@@ -19,10 +20,11 @@ public class PostServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         try {
-            req.setAttribute("posts", PsqlStore.instOf().findAllPosts());
+            req.setAttribute("posts", new ArrayList<>(PsqlStore.instOf().findAllPosts()));
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
+        req.setAttribute("user", req.getSession().getAttribute("user"));
         req.getRequestDispatcher("posts.jsp").forward(req, resp);
     }
 
